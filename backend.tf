819f364c123326cac1dc524ffcf5293ea8e2b70b
rutwik30usr <rutwiksatpute3034@gmail.com>
2025-07-04 15:20:06 +0530

    Last commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "ecp-120201"
-#     key    = "s3://ecp-120201/data/terraform.tfstate"
-#     region ="ap-south-1"  
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "ecp-120201"
+    key    = "s3://ecp-120201/data/terraform.tfstate"
+    region ="ap-south-1"  
+  }
+}
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "swstikroot" {
-  bucket = "tg-sastikroot1"
+# resource "aws_s3_bucket" "swstikroot" {
+#   bucket = "tg-sastikroot1"
 
-  tags = {
-    name        = "tg-sastikroot"
-    Environment = "dev"
-  }
+#   tags = {
+#     name        = "tg-sastikroot"
+#     Environment = "dev"
+#   }
 
-}
+# }
